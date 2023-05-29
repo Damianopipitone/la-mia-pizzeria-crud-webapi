4a44e976360b0f021a1f10d1600c69c9df46e040
Damianopipitone <damianopipitone33@gmail.com>
2023-05-29 16:04:53 +0200

    Aggiunto controller API GetPizzas()

---
+++ LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using LaMiaPizzeria.DataBase;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMiaPizzeria.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaAPIController : ControllerBase
+    {
+        public IActionResult GetPizzas()
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                List<PizzaModel> pizzas = db.Pizze.ToList();
+                return Ok(pizzas);
+            }
+        }
+    }
+}
